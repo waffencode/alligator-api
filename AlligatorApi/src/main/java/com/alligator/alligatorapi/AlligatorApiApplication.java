0cd4e514367385572a0a5ab725009ecd00c72879
Иван <ivanandrosovv@gmail.com>
2024-03-13 12:11:46 +0300

    Spring Initialization

---
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/AlligatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.alligator.alligatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlligatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlligatorApiApplication.class, args);
+    }
+
+}
---
+++ AlligatorApi/src/test/java/com/alligator/alligatorapi/AlligatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alligator.alligatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlligatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
