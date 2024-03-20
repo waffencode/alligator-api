8f0fe521acb87d1419e90221affaefa4e141a193
dictator_zx <ivanandrosovv@gmail.com>
2024-03-21 02:10:36 +0300

    Tasks secured - access only for BA

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/DeadlineRepository.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/DeadlineRepository.java
@@ -2,6 +2,8 @@ package com.alligator.alligatorapi.repository;
 
 import com.alligator.alligatorapi.entity.Deadline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
+@PreAuthorize("hasAuthority('ROLE_BUSINESS_ANALYTIC')")
 public interface DeadlineRepository extends JpaRepository<Deadline, Long> {
 }
--- AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/TaskDependencyRepository.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/TaskDependencyRepository.java
@@ -2,6 +2,8 @@ package com.alligator.alligatorapi.repository;
 
 import com.alligator.alligatorapi.entity.TaskDependency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
+@PreAuthorize("hasAuthority('ROLE_BUSINESS_ANALYTIC')")
 public interface TaskDependencyRepository extends JpaRepository<TaskDependency, Long> {
 }
