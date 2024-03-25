f9d7ed65530334e59197720d6ed82be3b44f5fe0
dictator_zx <ivanandrosovv@gmail.com>
2024-03-25 16:19:09 +0300

    Spring Security + Spring Data Rest first secure method

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/UserInfoRepository.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/UserInfoRepository.java
@@ -1,6 +1,7 @@
 package com.alligator.alligatorapi.repository;
 
 import com.alligator.alligatorapi.entity.UserInfo;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -10,5 +11,4 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     @Override
     @PreAuthorize("@securityService.validateUsernameSameAsPrincipal(#userInfo.user.username)")
     UserInfo save(@Param("userInfo") UserInfo userInfo);
-
 }
