d19ec8d6cc5fca05ae4f9f6af7bafdc2118faaab
Иван <ivanandrosovv@gmail.com>
2024-03-26 13:44:20 +0300

    Role repos re-secured

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/user/RoleRepository.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/repository/user/RoleRepository.java
@@ -5,11 +5,18 @@ import com.alligator.alligatorapi.entity.user.Role;
 import com.alligator.alligatorapi.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    @RestResource(exported = false)
+    <S extends Role> S save(S role);
+    @RestResource(exported = false)
+
+    <S extends Role> List<S> saveAll(Iterable<S> roles);
+
     boolean existsByName(RoleNames roleName);
 
     Role findByName(RoleNames roleNames);
