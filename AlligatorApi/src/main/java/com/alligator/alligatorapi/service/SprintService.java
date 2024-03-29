d7ecd046677f140d6ea3de7e9ef7e6b9c0c5766e
dictator_zx <ivanandrosovv@gmail.com>
2024-03-29 10:32:20 +0300

    Imports cleanup

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/service/SprintService.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/service/SprintService.java
@@ -2,13 +2,6 @@ package com.alligator.alligatorapi.service;
 
 import com.alligator.alligatorapi.entity.sprint.AssignedTask;
 import com.alligator.alligatorapi.entity.sprint.Sprint;
-import com.alligator.alligatorapi.entity.sprint.SprintTask;
-import com.alligator.alligatorapi.entity.sprint.SprintTaskRole;
-import com.alligator.alligatorapi.entity.team.TeamMember;
-import com.alligator.alligatorapi.entity.team.TeamMemberRole;
-import com.alligator.alligatorapi.entity.team.TeamRole;
-import com.alligator.alligatorapi.repository.sprint.SprintTaskRequiredRoleRepository;
-import com.alligator.alligatorapi.repository.team.TeamMemberRoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +12,7 @@ import java.util.logging.Logger;
 @Service
 @RequiredArgsConstructor
 public class SprintService {
-    
+
 
     public List<AssignedTask> suggestTaskAssignation(Sprint sprint) {
         Logger.getLogger(SprintService.class.getName()).info("This doesn't works yet...");
