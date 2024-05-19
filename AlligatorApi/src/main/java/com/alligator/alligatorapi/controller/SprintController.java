e94e537742938053c10cc4a4b0b0abe6f837f329
Dmitry Osobov <waffencode@gmail.com>
2024-05-19 16:18:35 +0300

    Attempt to fix TransientObjectException on /assign
    org.springframework.dao.InvalidDataAccessApiUsageException: org.hibernate.TransientObjectException: object references an unsaved transient instance - save the transient instance before flushing: com.alligator.alligatorapi.model.entity.sprint.Sprint

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/controller/SprintController.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/controller/SprintController.java
@@ -4,15 +4,18 @@ import com.alligator.alligatorapi.model.entity.sprint.Sprint;
 import com.alligator.alligatorapi.model.entity.team.TeamMember;
 import com.alligator.alligatorapi.service.SprintService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@Slf4j
 public class SprintController
 {
     private final SprintService sprintService;
@@ -20,7 +23,17 @@ public class SprintController
     @PostMapping(path = "/assign")
     @ResponseStatus(HttpStatus.OK)
     public String assignTasks(@RequestBody Sprint sprint) {
-        List<TeamMember> list = sprintService.suggestTaskAssignation(sprint);
+        log.info("Assigning tasks to sprint {}", sprint);
+        List<TeamMember> list = new ArrayList<>();
+
+        try {
+
+            list = sprintService.suggestTaskAssignation(sprint);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
 
         return list.toString();
     }
