360efff451fad4bea84749eb8ae97669c4a535fc
Dmitry Osobov <waffencode@gmail.com>
2024-05-22 01:04:33 +0300

    Extract getSprintTasks function

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/service/SprintService.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/service/SprintService.java
@@ -7,7 +7,6 @@ import com.alligator.alligatorapi.model.entity.sprint.SprintTask;
 import com.alligator.alligatorapi.model.entity.team.Team;
 import com.alligator.alligatorapi.model.entity.team.TeamMember;
 import com.alligator.alligatorapi.model.repository.sprint.AssignedTaskRepository;
-import com.alligator.alligatorapi.model.repository.sprint.SprintRepository;
 import com.alligator.alligatorapi.model.repository.sprint.SprintTaskRepository;
 import com.alligator.alligatorapi.model.repository.team.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -29,16 +28,12 @@ public class SprintService {
      * Proceeds with task assignment to the sprint. Returns a list of suggested tasks.
      */
     // Signature changed for test purposes.
-    public List<SprintTask> suggestTaskAssignation(Sprint sprint) {
+    public List<AssignedTask> doTaskAssignation(Sprint sprint) {
         /*
          1. Вытаскиваем таски из SprintTask в List<SprintTask>
          2. Проверка статуса задачи: назначена ли она, не завершена ли. Проверка зависимостей.
         */
-        List<SprintTask> allowedToAssignationTasks = sprintTaskRepository.findAllBySprint(sprint).stream()
-                .filter(task -> task.getTask().getState().equals(TaskState.TODO))
-                .filter(task -> taskService.taskHasUndoneDependencies(task.getTask()))
-                .sorted(Comparator.comparing(task -> task.getTask().getPriority()))
-                .toList();
+        List<SprintTask> allowedToAssignationTasks = getSprintTasks(sprint);
 
         Team sprintTeam = sprint.getTeam();
         List<TeamMember> teamMembers = teamMemberRepository.findAllByTeam(sprintTeam);
@@ -51,6 +46,7 @@ public class SprintService {
                 assignedTask.setTask(task);
                 assignedTask.setTeamMember(teamMember);
                 assignedTaskRepository.save(assignedTask);
+                assignedTasks.add(assignedTask);
             }
         }
 
@@ -70,6 +66,16 @@ public class SprintService {
         */
 
         // For test purposes.
-        return allowedToAssignationTasks;
+        return assignedTasks;
+    }
+
+    // Added for further decomposition.
+    public List<SprintTask> getSprintTasks(Sprint sprint)
+    {
+        return sprintTaskRepository.findAllBySprint(sprint).stream()
+                .filter(task -> task.getTask().getState().equals(TaskState.TODO))
+                .filter(task -> taskService.taskHasUndoneDependencies(task.getTask()))
+                .sorted(Comparator.comparing(task -> task.getTask().getPriority()))
+                .toList();
     }
 }
