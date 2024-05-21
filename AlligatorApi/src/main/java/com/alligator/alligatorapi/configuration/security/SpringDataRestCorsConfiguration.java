6bb0c9063fc37b04d9cda23f2926beb8e2ee9872
dictator_zx <ivanandrosovv@gmail.com>
2024-05-22 00:48:20 +0300

    upsie hehe, now fix

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/configuration/security/SpringDataRestCorsConfiguration.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/configuration/security/SpringDataRestCorsConfiguration.java
@@ -2,10 +2,11 @@ package com.alligator.alligatorapi.configuration.security;
 
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-@CrossOrigin
+@Component
 public class SpringDataRestCorsConfiguration implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
