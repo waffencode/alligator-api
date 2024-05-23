b5622256326260c0b997573a61d85f6887a5ba55
dictator_zx <ivanandrosovv@gmail.com>
2024-05-23 12:46:51 +0300

    Rename

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/configuration/security/SpringDataRestConfiguration.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/configuration/security/SpringDataRestConfiguration.java
@@ -3,11 +3,10 @@ package com.alligator.alligatorapi.configuration.security;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Component
-public class SpringDataRestCorsConfiguration implements RepositoryRestConfigurer {
+public class SpringDataRestConfiguration implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         cors.addMapping("/**")
