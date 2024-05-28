a01cf323f066882a3a43d986ce18d92d02a0a1d3
Dmitry Osobov <waffencode@gmail.com>
2024-05-29 00:09:34 +0300

    Enable QueryDSL in AssignedTaskRepository

--- AlligatorApi/src/main/java/com/alligator/alligatorapi/model/repository/sprint/AssignedTaskRepository.java
+++ AlligatorApi/src/main/java/com/alligator/alligatorapi/model/repository/sprint/AssignedTaskRepository.java
@@ -4,7 +4,8 @@ import com.alligator.alligatorapi.model.entity.sprint.AssignedTask;
 import com.alligator.alligatorapi.model.entity.task.Task;
 import com.alligator.alligatorapi.model.entity.team.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface AssignedTaskRepository extends JpaRepository<AssignedTask, Long> {
+public interface AssignedTaskRepository extends JpaRepository<AssignedTask, Long>, QuerydslPredicateExecutor<AssignedTask> {
     Boolean existsByTaskAndTeamMember(Task task, TeamMember member);
 }
